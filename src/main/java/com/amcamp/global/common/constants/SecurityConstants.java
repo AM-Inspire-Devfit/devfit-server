ac0edfc56d42365232831f5de9c6781ec01d5d60
현태 최 <ht3064@naver.com>
2025-01-31 18:43:41 +0900

    feat: 구글, 카카오 인증 서버로부터 ID 토큰 발급받는 기능 구현

---
+++ src/main/java/com/amcamp/global/common/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.amcamp.global.common.constants;
+
+public final class SecurityConstants {
+	public static final String KAKAO_LOGIN_URL = "https://kauth.kakao.com";
+	public static final String KAKAO_LOGIN_ENDPOINT = "/oauth/token";
+
+	public static final String GOOGLE_LOGIN_URL = "https://oauth2.googleapis.com";
+	public static final String GOOGLE_LOGIN_ENDPOINT = "/token";
+}
