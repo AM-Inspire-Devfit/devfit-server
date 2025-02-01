67d9ecc5dca50fdede1ad38b01e174756d2570d2
현태 최 <ht3064@naver.com>
2025-02-02 04:57:18 +0900

    refactor: 리프레시 토큰을 쿠키로 관리하도록 개선

--- src/main/java/com/amcamp/domain/auth/api/AuthController.java
+++ src/main/java/com/amcamp/domain/auth/api/AuthController.java
@@ -4,9 +4,12 @@ import com.amcamp.domain.auth.application.AuthService;
 import com.amcamp.domain.auth.domain.OauthProvider;
 import com.amcamp.domain.auth.dto.request.AuthCodeRequest;
 import com.amcamp.domain.auth.dto.response.SocialLoginResponse;
+import com.amcamp.global.util.CookieUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "인증 API", description = "인증 관련 API입니다.")
@@ -16,12 +19,18 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieUtil cookieUtil;
 
     @Operation(summary = "회원가입 및 로그인", description = "회원가입 및 로그인을 진행합니다.")
     @PostMapping("/social-login")
-    public SocialLoginResponse memberSocialLogin(
+    public ResponseEntity<SocialLoginResponse> memberSocialLogin(
             @RequestParam(name = "oauthProvider") OauthProvider provider,
             @RequestBody AuthCodeRequest request) {
-        return authService.socialLoginMember(request, provider);
+        SocialLoginResponse response = authService.socialLoginMember(request, provider);
+
+        String refreshToken = response.refreshToken();
+        HttpHeaders headers = cookieUtil.generateRefreshTokenCookie(refreshToken);
+
+        return ResponseEntity.ok().headers(headers).body(response);
     }
 }
