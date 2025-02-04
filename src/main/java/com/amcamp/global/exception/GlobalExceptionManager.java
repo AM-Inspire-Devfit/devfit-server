f396d6cf87d3d31631cfa885e93350d8df2f4d8c
현태 최 <ht3064@naver.com>
2025-02-04 10:48:05 +0900

    refactor: 사용하지 않는 AuthException 제거

--- src/main/java/com/amcamp/global/exception/GlobalExceptionManager.java
+++ src/main/java/com/amcamp/global/exception/GlobalExceptionManager.java
@@ -1,14 +1,13 @@
 package com.amcamp.global.exception;
 
 import com.amcamp.global.common.response.CommonResponse;
-import com.amcamp.global.exception.errorcode.AuthException;
 import com.amcamp.global.exception.errorcode.BaseErrorCode;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.amcamp")
 public class GlobalExceptionManager {
-	@ExceptionHandler(AuthException.class)
+	@ExceptionHandler(CommonException.class)
 	public CommonResponse<?> commonExceptionHandler(CommonException e){
 		BaseErrorCode baseErrorCode = e.getErrorCode();
 		ErrorDetail errorDetail = ErrorDetail.builder()
