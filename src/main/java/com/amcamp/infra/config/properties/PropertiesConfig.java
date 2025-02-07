828d2dcfc533334ac5fc4837128a1a51a9e2daa5
현태 최 <ht3064@naver.com>
2025-02-07 13:53:23 +0900

    chore: s3 properties 추가

--- src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
+++ src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
@@ -4,6 +4,7 @@ import com.amcamp.infra.config.jwt.JwtProperties;
 import com.amcamp.infra.config.oauth.GoogleProperties;
 import com.amcamp.infra.config.oauth.KakaoProperties;
 import com.amcamp.infra.config.redis.RedisProperties;
+import com.amcamp.infra.config.s3.S3Properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
         GoogleProperties.class,
         KakaoProperties.class,
         JwtProperties.class,
+		S3Properties.class
 })
 @Configuration
 public class PropertiesConfig {}
---
+++ src/main/java/com/amcamp/infra/config/s3/S3Properties.java
@@ -0,0 +1,12 @@
+package com.amcamp.infra.config.s3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("cloud.aws")
+public record S3Properties(
+	String region,
+	String accessKey,
+	String secretKey,
+	String bucket,
+	String endpoint) {
+}
