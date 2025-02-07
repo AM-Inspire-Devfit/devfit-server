159884da5743571c003116443bf1e77f069e96f8
Subin Cho <chosubinn@ewhain.net>
2025-02-07 15:48:42 +0900

    test: 팀 서비스 테스트 수정

--- src/test/java/com/amcamp/domain/team/TeamServiceTest.java
+++ src/test/java/com/amcamp/domain/team/TeamServiceTest.java
@@ -4,6 +4,7 @@ import com.amcamp.domain.member.dao.MemberRepository;
 import com.amcamp.domain.member.domain.Member;
 import com.amcamp.domain.member.domain.MemberRole;
 import com.amcamp.domain.member.domain.MemberStatus;
+import com.amcamp.domain.team.application.InviteCodeService;
 import com.amcamp.domain.team.application.TeamService;
 import com.amcamp.domain.team.domain.Team;
 import com.amcamp.domain.team.domain.Participant;
@@ -17,10 +18,13 @@ import com.amcamp.global.exception.errorcode.TeamErrorCode;
 import com.amcamp.global.security.PrincipalDetails;
 import com.amcamp.global.util.MemberUtil;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -49,119 +53,100 @@ class TeamServiceTest {
 	private TeamService teamService;
 	@Autowired
 	private MemberUtil memberUtil;
+	@Autowired
+	private InviteCodeService inviteCodeService;
+	@Autowired
+	StringRedisTemplate redisTemplate;
 
-	@Test
-	@Transactional
-	void 팀_생성_후_참가자를_조회한다() {
-		// given
-		UserDetails userDetails = new PrincipalDetails(1L, MemberRole.USER);
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(token);
+	@Nested
+	class 팀_생성_시{
+		@Test
+		@Transactional
+		void 팀_생성_후_참가자_조회() {
+			// given
+			UserDetails userDetails = new PrincipalDetails(1L, MemberRole.USER);
+			UsernamePasswordAuthenticationToken token =
+				new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(token);
 
-		Member savedMember = memberRepository.save(Member.createMember("username", null, null));  // 멤버 저장
+			Member savedMember = memberRepository.save(Member.createMember("username", null, null));  // 멤버 저장
 
-		String teamName = "팀 이름";
-		String teamDescription = "팀 설명";
+			String teamName = "팀 이름";
+			String teamDescription = "팀 설명";
 
-		//when
-		Team savedTeam = teamService.saveTeam(teamName, teamDescription);
+			//when
+			Team savedTeam = teamService.saveTeam(teamName, teamDescription);
 
-		// then
-		Team getTeam = teamRepository.findById(savedTeam.getId())
-			.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_NOT_FOUND));
+			// then
+			Team getTeam = teamRepository.findById(savedTeam.getId())
+				.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_NOT_FOUND));
 
-		Assertions.assertNotNull(savedTeam);
-		Assertions.assertEquals(teamName, getTeam.getTeam_name());
-		Assertions.assertEquals(teamDescription, getTeam.getTeam_description());
+			Assertions.assertNotNull(savedTeam);
+			Assertions.assertEquals(teamName, getTeam.getTeam_name());
+			Assertions.assertEquals(teamDescription, getTeam.getTeam_description());
 
-		Participant getParticipant = participantRepository.findByMemberAndTeam(savedMember, getTeam)
-			.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_PARTICIPANT_NOT_FOUND));
+			Participant getParticipant = participantRepository.findByMemberAndTeam(savedMember, getTeam)
+				.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_PARTICIPANT_NOT_FOUND));
 
-		Assertions.assertNotNull(getParticipant);
-		Assertions.assertEquals(savedMember.getId(), getParticipant.getMember().getId());
-		Assertions.assertEquals(savedTeam.getId(), getParticipant.getTeam().getId());
-		Assertions.assertEquals(ParticipantRole.ADMIN, getParticipant.getRole());  // ADMIN 역할 확인
+			Assertions.assertNotNull(getParticipant);
+			Assertions.assertEquals(savedMember.getId(), getParticipant.getMember().getId());
+			Assertions.assertEquals(savedTeam.getId(), getParticipant.getTeam().getId());
+			Assertions.assertEquals(ParticipantRole.ADMIN, getParticipant.getRole());  // ADMIN 역할 확인
+		}
 	}
 
-	@Test
-	@Transactional
-	void 이미_참여한_멤버가_팀에_참여한다(){
-		//given
-		UserDetails userDetails = new PrincipalDetails(1L, MemberRole.USER);
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(token);
+	@Nested
+	class 팀_참여_시 {
+		@Test
+		@Transactional
+		void 이미_참여한_멤버가_팀에_참여한다() {
+			//given
+			UserDetails userDetails = new PrincipalDetails(1L, MemberRole.USER);
+			UsernamePasswordAuthenticationToken token =
+				new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(token);
 
-		Member savedMember = memberRepository.save(Member.createMember("username", null, null));  // 멤버 저장
-		Team savedTeam = teamService.saveTeam("팀 이름", "팀 설명");
+			Member savedMember = memberRepository.save(Member.createMember("username", null, null));  // 멤버 저장
+			Team savedTeam = teamService.saveTeam("팀 이름", "팀 설명");
+			String inviteCode = inviteCodeService.generateCode(savedTeam.getId());
 
-		//when & then
-		CommonException exception = assertThrows(CommonException.class, () -> teamService.joinTeam(savedTeam.getId()));
-		Assertions.assertEquals(TeamErrorCode.ALREADY_PARTICIPANT, exception.getErrorCode());
+			//when & then
+			CommonException exception = assertThrows(CommonException.class, () -> teamService.joinTeam(inviteCode));
+			Assertions.assertEquals(TeamErrorCode.ALREADY_PARTICIPANT, exception.getErrorCode());
+		}
+
+
+		@Test
+		@Transactional
+		void 새롭게_참여하는_멤버가_팀에_참여한다() {
+			// given
+			// 1. savedAdmin 로그인 처리 후 팀 생성
+			Member savedAdmin = memberRepository.save(Member.createMember("admin", null, null));
+			UserDetails userDetails = new PrincipalDetails(savedAdmin.getId(), savedAdmin.getRole());
+			UsernamePasswordAuthenticationToken token =
+				new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(token);
+			Team savedTeam = teamService.saveTeam("팀 이름", "팀 설명");
+			String inviteCode = inviteCodeService.generateCode(savedTeam.getId());
+
+			// 2. savedMember 로그인 처리 후 팀 참여
+			Member savedMember = memberRepository.save(Member.createMember("member", null, null));
+			UserDetails newUserDetails = new PrincipalDetails(savedMember.getId(), savedMember.getRole());
+			UsernamePasswordAuthenticationToken newToken =
+				new UsernamePasswordAuthenticationToken(newUserDetails, null, newUserDetails.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(newToken);
+
+			// when
+			teamService.joinTeam(inviteCode);
+
+			// then
+			Participant participant = participantRepository.findByMemberAndTeam(savedMember, savedTeam)
+				.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_PARTICIPANT_NOT_FOUND));
+
+			Assertions.assertEquals(savedMember, participant.getMember());
+			Assertions.assertEquals(participant.getRole(), ParticipantRole.USER);
+			Assertions.assertEquals(savedTeam, participant.getTeam());
+		}
 	}
 
-	@Test
-	@Transactional
-	void 새롭게_참여하는_멤버가_팀에_참여한다(){
-		// given
-		// 1. savedAdmin 로그인 처리 후 팀 생성
-		Member savedAdmin = memberRepository.save(Member.createMember("admin", null, null));
-		UserDetails userDetails = new PrincipalDetails(savedAdmin.getId(), savedAdmin.getRole());
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(token);
-		Team savedTeam = teamService.saveTeam("팀 이름", "팀 설명");
-
-		// 2. savedMember 로그인 처리 후 팀 참여
-		Member savedMember = memberRepository.save(Member.createMember("member", null, null));
-		UserDetails newUserDetails = new PrincipalDetails(savedMember.getId(), savedMember.getRole());
-		UsernamePasswordAuthenticationToken newToken =
-			new UsernamePasswordAuthenticationToken(newUserDetails, null, newUserDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(newToken);
-
-		// when
-		teamService.joinTeam(savedTeam.getId());
-
-		// then
-		Participant participant = participantRepository.findByMemberAndTeam(savedMember, savedTeam)
-			.orElseThrow(() -> new CommonException(TeamErrorCode.TEAM_PARTICIPANT_NOT_FOUND));
-
-		Assertions.assertEquals(savedMember, participant.getMember());
-		Assertions.assertEquals(participant.getRole(), ParticipantRole.USER);
-		Assertions.assertEquals(savedTeam, participant.getTeam());
-	}
-	@Test
-	@Transactional
-	void 팀이_존재_하지_않는데_팀에_참여(){
-		//given
-		UserDetails userDetails = new PrincipalDetails(1L, MemberRole.USER);
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(token);
-
-		Member savedMember = memberRepository.save(Member.createMember("username", null, null));  // 멤버 저장
-		Long nonExistentTeamId = 9999L;
-
-		// when & then
-		Assertions.assertEquals(TeamErrorCode.TEAM_NOT_FOUND, assertThrows(CommonException.class, () -> {
-			teamService.joinTeam(nonExistentTeamId);}).getErrorCode());
-	}
-	@Test
-	@Transactional
-	void 로그인_하지_않은_멤버가_팀에_참여(){
-		// given
-		Member savedAdmin = memberRepository.save(Member.createMember("admin", null, null));
-		UserDetails userDetails = new PrincipalDetails(savedAdmin.getId(), savedAdmin.getRole());
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(token);
-		Team savedTeam = teamService.saveTeam("팀 이름", "팀 설명");
-
-		SecurityContextHolder.getContext().setAuthentication(null);
-
-		// when & then
-		Assertions.assertEquals(AuthErrorCode.AUTH_NOT_FOUND, assertThrows(CommonException.class, () -> {
-			teamService.joinTeam(savedTeam.getId());}).getErrorCode());
-	}
 }
