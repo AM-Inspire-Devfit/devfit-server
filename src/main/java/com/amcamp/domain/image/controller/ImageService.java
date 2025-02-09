4be89abede90ca2d7d470a1ce4b5ec7bc840191e
현태 최 <ht3064@naver.com>
2025-02-09 16:15:24 +0900

    feat: 회원 프로필 이미지 Presigned URL 생성

---
+++ src/main/java/com/amcamp/domain/image/controller/ImageService.java
@@ -0,0 +1,83 @@
+package com.amcamp.domain.image.controller;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amcamp.domain.image.dao.ImageRepository;
+import com.amcamp.domain.image.domain.Image;
+import com.amcamp.domain.image.domain.ImageFileExtension;
+import com.amcamp.domain.image.dto.request.MemberImageUploadRequest;
+import com.amcamp.domain.image.dto.response.PresignedUrlResponse;
+import com.amcamp.domain.member.domain.Member;
+import com.amcamp.global.util.MemberUtil;
+import com.amcamp.infra.config.s3.S3Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final MemberUtil memberUtil;
+    private final S3Properties s3Properties;
+    private final ImageRepository imageRepository;
+    private final AmazonS3 amazonS3;
+
+    public PresignedUrlResponse createMemberImagePresignedUrl(MemberImageUploadRequest request) {
+        final Member currentMember = memberUtil.getCurrentMember();
+
+        String imageKey = generateUUID();
+        String imageName = createImageFileName(currentMember.getId(), imageKey, request.imageFileExtension());
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                generatePresignedUrlRequest(s3Properties.bucket(), imageName, request.imageFileExtension().getExtension());
+
+        String presignedUrl = amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString();
+
+        imageRepository.save(
+                Image.createImage(
+                        currentMember.getId(),
+                        imageKey,
+                        request.imageFileExtension()));
+
+        return new PresignedUrlResponse(presignedUrl);
+    }
+
+    private GeneratePresignedUrlRequest generatePresignedUrlRequest(
+            String bucket, String imageName, String imageFileExtension) {
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucket, imageName, HttpMethod.PUT)
+                        .withKey(imageName)
+                        .withContentType("image/" + imageFileExtension)
+                        .withExpiration(getPresignedUrlExpiration());
+
+        generatePresignedUrlRequest.addRequestParameter(
+                Headers.S3_CANNED_ACL, CannedAccessControlList.PublicRead.toString());
+
+        return generatePresignedUrlRequest;
+    }
+
+    private String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    private String createImageFileName(Long memberId, String imageKey, ImageFileExtension imageFileExtension) {
+        return memberId + "/" + imageKey + "." + imageFileExtension.getExtension();
+    }
+
+    private Date getPresignedUrlExpiration() {
+        Date expiration = new Date();
+        long expTime = expiration.getTime();
+        expTime += TimeUnit.MINUTES.toMillis(3);
+        expiration.setTime(expTime);
+
+        return expiration;
+    }
+}
---
+++ src/main/java/com/amcamp/domain/image/domain/ImageFileExtension.java
@@ -0,0 +1,16 @@
+package com.amcamp.domain.image.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ImageFileExtension {
+
+	PNG("png"),
+	JPG("jpg"),
+	JPEG("jpeg"),
+	;
+
+	private final String extension;
+}
