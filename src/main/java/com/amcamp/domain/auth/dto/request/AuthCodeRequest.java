dc05b4ba4fb40e9b7535c4a49dcff0f79faaaad8
현태 최 <ht3064@naver.com>
2025-02-10 21:29:29 +0900

    fix: request dto valid @NotEmpty로 수정

--- src/main/java/com/amcamp/domain/auth/dto/request/AuthCodeRequest.java
+++ src/main/java/com/amcamp/domain/auth/dto/request/AuthCodeRequest.java
@@ -1,8 +1,8 @@
 package com.amcamp.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record AuthCodeRequest(
-        @NotNull(message = "인증코드는 필수입니다") @Schema(description = "구글, 카카오 로그인을 통한 인증코드")
+        @NotBlank(message = "인증코드는 필수입니다") @Schema(description = "구글, 카카오 로그인을 통한 인증코드")
                 String code) {}
