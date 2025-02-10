3a7bc1ff478eacb570122eb86ea8d5d86b2936f6
현태 최 <ht3064@naver.com>
2025-02-10 11:07:47 +0900

    refactor: 잘못 추가된 패키지 제거

--- test/src/Student.java
+++
@@ -1,8 +0,0 @@
-public class Student extends Person {
-    public int number;
-
-    public void goSchool() {
-        System.out.println("등교하기");
-    }
-
-}
--- test/src/Worker.java
+++
@@ -1,7 +0,0 @@
-public class Worker extends Person {
-    private int url;
-
-    private void goWork() {
-        System.out.println("출근하기");
-    }
-}
