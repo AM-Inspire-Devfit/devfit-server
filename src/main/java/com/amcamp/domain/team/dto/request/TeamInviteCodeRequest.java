10b11a85ec2f96e3d55136b8e360a76c726f59fa
Subin Cho <chosubinn@ewhain.net>
2025-02-12 09:44:08 +0900

    fix: 초대코드 사이즈 제한 삭제

--- src/main/java/com/amcamp/domain/team/dto/request/TeamInviteCodeRequest.java
+++ src/main/java/com/amcamp/domain/team/dto/request/TeamInviteCodeRequest.java
@@ -2,10 +2,6 @@ package com.amcamp.domain.team.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record TeamInviteCodeRequest(
-        @NotBlank
-                @Size(min = 8, max = 8, message = "8자리 초대 코드를 입력해주세요.")
-                @Schema(description = "팀 코드", example = "FFbKeJvJ")
-                String inviteCode) {}
+        @NotBlank @Schema(description = "팀 코드", example = "FFbKeJvJ") String inviteCode) {}
