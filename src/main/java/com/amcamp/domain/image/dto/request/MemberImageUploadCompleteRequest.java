9f337c1cf612cda82b9bedbd44e0a5411df3494f
현태 최 <ht3064@naver.com>
2025-02-12 10:05:05 +0900

    fix: image request dto validation 수정

--- src/main/java/com/amcamp/domain/image/dto/request/MemberImageUploadCompleteRequest.java
+++ src/main/java/com/amcamp/domain/image/dto/request/MemberImageUploadCompleteRequest.java
@@ -2,9 +2,9 @@ package com.amcamp.domain.image.dto.request;
 
 import com.amcamp.domain.image.domain.ImageFileExtension;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record MemberImageUploadCompleteRequest(
-        @NotBlank(message = "이미지 파일 확장자는 비워둘 수 없습니다.")
+        @NotNull(message = "이미지 파일 확장자는 비워둘 수 없습니다.")
                 @Schema(description = "이미지 파일 확장자", defaultValue = "JPEG")
                 ImageFileExtension imageFileExtension) {}
