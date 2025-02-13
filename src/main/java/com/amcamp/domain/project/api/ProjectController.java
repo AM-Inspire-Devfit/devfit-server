e52230366b4e1f33ba885c5586a36eb764d33798
willjsw <sunwoo1137@gmail.com>
2025-02-13 16:13:50 +0900

    fix: ProjectController API @Valid 어노테이션 제거

--- src/main/java/com/amcamp/domain/project/api/ProjectController.java
+++ src/main/java/com/amcamp/domain/project/api/ProjectController.java
@@ -6,7 +6,6 @@ import com.amcamp.domain.project.dto.response.ProjectInfoResponse;
 import com.amcamp.domain.project.dto.response.ProjectListInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,13 +31,13 @@ public class ProjectController {
             summary = "전체 프로젝트 목록 조회",
             description = "팀 ID를 통해 사용자가 참여 중인 프로젝트와 참여 중이지 않은 프로젝트를 나누어 조회합니다.")
     @GetMapping("/{teamId}/list")
-    public ProjectListInfoResponse projectListInfo(@Valid @PathVariable Long teamId) {
+    public ProjectListInfoResponse projectListInfo(@PathVariable Long teamId) {
         return projectService.getProjectListInfo(teamId);
     }
 
     @Operation(summary = "프로젝트 조회", description = "프로젝트 ID를 통해 프로젝트 정보를 조회합니다.")
     @GetMapping("/{projectId}")
-    public ProjectInfoResponse projectInfo(@Valid @PathVariable Long projectId) {
+    public ProjectInfoResponse projectInfo(@PathVariable Long projectId) {
         return projectService.getProjectInfo(projectId);
     }
 }
