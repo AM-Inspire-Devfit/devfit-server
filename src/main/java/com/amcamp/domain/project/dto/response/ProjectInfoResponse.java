dd246045c7d9ec10f5d29a0b07438b31abf4b079
willjsw <sunwoo1137@gmail.com>
2025-02-16 01:13:32 +0900

    fix: project dto schema 수정- projectDescription의 description

--- src/main/java/com/amcamp/domain/project/dto/response/ProjectInfoResponse.java
+++ src/main/java/com/amcamp/domain/project/dto/response/ProjectInfoResponse.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public record ProjectInfoResponse(
         @Schema(description = "프로젝트 아이디", example = "1") Long projectId,
         @Schema(description = "프로젝트 제목", example = "Devfit") String projectTitle,
-        @Schema(description = "프로젝트 목표", example = "LG CNS AM Inspire Camp 사이드 프로젝트")
+        @Schema(description = "프로젝트 설명", example = "LG CNS AM Inspire Camp 사이드 프로젝트")
                 String projectDescription,
         @Schema(description = "프로젝트 목표", example = "개발자 건강을 위한 협업 툴 개발") String projectGoal,
         @Schema(description = "프로젝트 시작 날짜", example = "2024-01-01T00:00") LocalDateTime startDt,
