878e56707ef2ce55ffc04b65691dc2aa9258caba
Subin Cho <chosubinn@ewhain.net>
2025-03-05 15:59:03 +0900

    refactor: 태스크 에러코드 메시지 수정

--- src/main/java/com/amcamp/global/exception/errorcode/TaskErrorCode.java
+++ src/main/java/com/amcamp/global/exception/errorcode/TaskErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum TaskErrorCode implements BaseErrorCode {
-    TASK_NOT_FOUND(HttpStatus.BAD_REQUEST, "task를 찾을 수 없습니다."),
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "task를 찾을 수 없습니다."),
     TASK_MODIFY_FORBIDDEN(HttpStatus.FORBIDDEN, "task 수정·삭제 권한이 없습니다. ");
 
     private final HttpStatus httpStatus;
