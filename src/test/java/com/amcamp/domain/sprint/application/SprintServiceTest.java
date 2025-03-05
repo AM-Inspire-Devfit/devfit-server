deadee72dbe55e308479f31231002740fae3fe78
현태 최 <ht3064@naver.com>
2025-03-05 18:38:06 +0900

    test: 스프린트 생성 테스트 작성

---
+++ src/test/java/com/amcamp/domain/sprint/application/SprintServiceTest.java
@@ -0,0 +1,114 @@
+package com.amcamp.domain.sprint.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.amcamp.IntegrationTest;
+import com.amcamp.domain.member.dao.MemberRepository;
+import com.amcamp.domain.member.domain.Member;
+import com.amcamp.domain.member.domain.OauthInfo;
+import com.amcamp.domain.project.dao.ProjectParticipantRepository;
+import com.amcamp.domain.project.dao.ProjectRepository;
+import com.amcamp.domain.project.domain.Project;
+import com.amcamp.domain.project.domain.ProjectParticipant;
+import com.amcamp.domain.project.domain.ProjectParticipantRole;
+import com.amcamp.domain.sprint.dao.SprintRepository;
+import com.amcamp.domain.sprint.dto.request.SprintCreateRequest;
+import com.amcamp.domain.sprint.dto.response.SprintInfoResponse;
+import com.amcamp.domain.team.dao.TeamParticipantRepository;
+import com.amcamp.domain.team.dao.TeamRepository;
+import com.amcamp.domain.team.domain.Team;
+import com.amcamp.domain.team.domain.TeamParticipant;
+import com.amcamp.domain.team.domain.TeamParticipantRole;
+import com.amcamp.global.exception.CommonException;
+import com.amcamp.global.exception.errorcode.GlobalErrorCode;
+import com.amcamp.global.security.PrincipalDetails;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SprintServiceTest extends IntegrationTest {
+
+    private final LocalDate startDt = LocalDate.of(2026, 1, 2);
+    private final LocalDate dueDt = LocalDate.of(2026, 3, 1);
+
+    @Autowired private SprintService sprintService;
+    @Autowired private SprintRepository sprintRepository;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private TeamRepository teamRepository;
+    @Autowired private TeamParticipantRepository teamParticipantRepository;
+    @Autowired private ProjectRepository projectRepository;
+    @Autowired private ProjectParticipantRepository projectParticipantRepository;
+
+    private Project project;
+
+    @BeforeEach
+    void setUp() {
+        Member member =
+                memberRepository.save(
+                        Member.createMember(
+                                "testNickname",
+                                "testProfileImageUrl",
+                                OauthInfo.createOauthInfo("testOauthId", "testOauthProvider")));
+
+        UserDetails userDetails = new PrincipalDetails(member.getId(), member.getRole());
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        Team team = Team.createTeam("testName", "testDescription");
+        teamRepository.save(team);
+        TeamParticipant teamParticipant =
+                TeamParticipant.createParticipant(member, team, TeamParticipantRole.ADMIN);
+        teamParticipantRepository.save(teamParticipant);
+
+        project =
+                Project.createProject(
+                        team, "testTitle", "testDescription", "testGoal", startDt, dueDt);
+        projectRepository.save(project);
+        ProjectParticipant projectParticipant =
+                ProjectParticipant.createProjectParticipant(
+                        teamParticipant, project, ProjectParticipantRole.ADMIN);
+        projectParticipantRepository.save(projectParticipant);
+    }
+
+    @Nested
+    class 스프린트_생성할_때 {
+        @Test
+        void 프로젝트_기간_내라면_스프린트를_생성한다() {
+            // given
+            SprintCreateRequest request =
+                    new SprintCreateRequest(project.getId(), "testGoal", startDt, dueDt);
+
+            // when
+            SprintInfoResponse response = sprintService.createSprint(request);
+
+            // then
+            assertThat(response.goal()).isEqualTo("testGoal");
+            assertThat(response.startDt()).isEqualTo(LocalDate.of(2026, 1, 2));
+            assertThat(response.dueDt()).isEqualTo(LocalDate.of(2026, 3, 1));
+        }
+
+        @Test
+        void 프로젝트_기간_외라면_예외가_발생한다() {
+            // given
+            SprintCreateRequest request =
+                    new SprintCreateRequest(
+                            project.getId(),
+                            "testGoal",
+                            LocalDate.of(2026, 1, 1),
+                            LocalDate.of(2026, 3, 2));
+
+            // when & then
+            assertThatThrownBy(() -> sprintService.createSprint(request))
+                    .isInstanceOf(CommonException.class)
+                    .hasMessage(GlobalErrorCode.INVALID_DATE_ERROR.getMessage());
+        }
+    }
+}
