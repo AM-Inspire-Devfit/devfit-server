3336e5effd8034d5abb5568b27db1a8323d1752f
현태 최 <ht3064@naver.com>
2025-03-08 21:32:01 +0900

    refactor: openai 도입에 따른 feign config 수정

--- src/main/java/com/amcamp/global/config/feign/FeignConfig.java
+++ src/main/java/com/amcamp/global/config/feign/FeignConfig.java
@@ -12,11 +12,18 @@ public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
-            requestTemplate.header(
-                    "Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+            if (requestTemplate.url().contains("kakao")
+                    || requestTemplate.url().contains("google")) {
+                requestTemplate.header(
+                        "Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
 
-            if (requestTemplate.body() == null) {
-                requestTemplate.body("");
+                if (requestTemplate.body() == null) {
+                    requestTemplate.body("");
+                }
+            }
+
+            if (requestTemplate.url().contains("openai")) {
+                requestTemplate.header("Content-Type", "application/json");
             }
         };
     }
