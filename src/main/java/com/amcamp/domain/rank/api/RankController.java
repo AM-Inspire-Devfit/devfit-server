9a26a62605b128129f4134ce037bdc587e8b880c
Subin Cho <chosubinn@ewhain.net>
2025-03-11 11:15:59 +0900

    refactor: api description 수정

--- src/main/java/com/amcamp/domain/rank/api/RankController.java
+++ src/main/java/com/amcamp/domain/rank/api/RankController.java
@@ -19,13 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RankController {
     private final RankService rankService;
 
-    @Operation(summary = "개별 회원 기여도 조회", description = "")
+    @Operation(summary = "개별 회원 기여도 조회", description = "스프린트별 개별 회원의 기여도를 조회합니다.")
     @GetMapping("/{sprintId}/me")
     public BasicRankInfoResponse rankByMember(@PathVariable Long sprintId) {
         return rankService.getRankByMember(sprintId);
     }
 
-    @Operation(summary = "프로젝트 내 회원 기여도 조회", description = "")
+    @Operation(summary = "프로젝트 내 회원 기여도 조회", description = "프로젝트 페이지에서 스프린트별 기여도를 조회합니다.")
     @GetMapping("/{sprintId}")
     public List<RankInfoResponse> rankBySprint(@PathVariable Long sprintId) {
         return rankService.getRankBySprint(sprintId);
