9937653209995169e736749c95abb123dcb948e5
willjsw <sunwoo1137@gmail.com>
2025-03-18 13:09:19 +0900

    refactor: mapping url 추가

--- src/main/java/com/amcamp/domain/meeting/api/MeetingController.java
+++ src/main/java/com/amcamp/domain/meeting/api/MeetingController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "8. 팀 미팅 API", description = "팀 미팅 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/meetings")
 public class MeetingController {
 
     private final MeetingService meetingService;
 
     @Operation(summary = "미팅 생성", description = "새로운 미팅을 생성합니다.")
-    @PostMapping
+    @PostMapping("/{meetingId}")
     public ResponseEntity<Void> meetingCreate(@RequestBody MeetingCreateRequest request) {
         meetingService.createMeeting(request);
         return ResponseEntity.ok().build();
