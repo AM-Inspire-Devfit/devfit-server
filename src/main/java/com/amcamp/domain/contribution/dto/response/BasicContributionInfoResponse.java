1316d4465f7824a24549a93aa591328ea114685c
Subin Cho <chosubinn@ewhain.net>
2025-03-20 14:46:10 +0900

    fix: contribution dto 수정

--- src/main/java/com/amcamp/domain/contribution/dto/response/BasicContributionInfoResponse.java
+++ src/main/java/com/amcamp/domain/contribution/dto/response/BasicContributionInfoResponse.java
@@ -4,16 +4,16 @@ import com.amcamp.domain.contribution.domain.Contribution;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record BasicContributionInfoResponse(
-        @Schema(description = "랭크 아이디", example = "1") Long rankId,
+        @Schema(description = "기여도 아이디", example = "1") Long contributionId,
         @Schema(description = "스프린트 아이디", example = "1") Long sprintId,
         @Schema(description = "멤버 아이디", example = "1") Long memberId,
-        @Schema(description = "기여도", example = "68.1") Double score) {
+        @Schema(description = "기여도 점수", example = "68.1") Double score) {
 
     public static BasicContributionInfoResponse from(Contribution contribution) {
         return new BasicContributionInfoResponse(
                 contribution.getId(),
                 contribution.getSprint().getId(),
-                contribution.getParticipant().getTeamParticipant().getMember().getId(),
+                contribution.getParticipant().getId(),
                 contribution.getScore());
     }
 }
--- src/main/java/com/amcamp/domain/contribution/dto/response/ContributionInfoResponse.java
+++ src/main/java/com/amcamp/domain/contribution/dto/response/ContributionInfoResponse.java
@@ -4,19 +4,19 @@ import com.amcamp.domain.contribution.domain.Contribution;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ContributionInfoResponse(
-        @Schema(description = "랭크 아이디", example = "1") Long rankId,
+        @Schema(description = "기여도 아이디", example = "1") Long contributionId,
         @Schema(description = "스프린트 아이디", example = "1") Long sprintId,
         @Schema(description = "멤버 아이디", example = "1") Long memberId,
         @Schema(description = "멤버 닉네임", example = "최현태") String nickname,
         @Schema(description = "멤버 프로필 url", example = "Presigned URL") String profileImageUrl,
-        @Schema(description = "기여도", example = "68.1") Double score) {
+        @Schema(description = "기여도 점수", example = "68.1") Double score) {
     public static ContributionInfoResponse from(Contribution contribution) {
         return new ContributionInfoResponse(
                 contribution.getId(),
                 contribution.getSprint().getId(),
-                contribution.getParticipant().getTeamParticipant().getMember().getId(),
-                contribution.getParticipant().getTeamParticipant().getMember().getNickname(),
-                contribution.getParticipant().getTeamParticipant().getMember().getProfileImageUrl(),
+                contribution.getParticipant().getId(),
+                contribution.getParticipant().getProjectNickname(),
+                contribution.getParticipant().getProjectProfile(),
                 contribution.getScore());
     }
 }
