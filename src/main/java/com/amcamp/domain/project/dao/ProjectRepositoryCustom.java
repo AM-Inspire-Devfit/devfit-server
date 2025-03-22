311b21b87a1fd1616868f3dcc69c3f179d47e962
willjsw <sunwoo1137@gmail.com>
2025-03-22 12:28:41 +0900

    refactor: projectListInfoResponse 요청/응답 형식 수정 및 isParticipant, isAdmin 응답필드 추가

--- src/main/java/com/amcamp/domain/project/dao/ProjectRepositoryCustom.java
+++ src/main/java/com/amcamp/domain/project/dao/ProjectRepositoryCustom.java
@@ -1,15 +1,11 @@
 package com.amcamp.domain.project.dao;
 
-import com.amcamp.domain.project.dto.response.ProjectInfoResponse;
+import com.amcamp.domain.project.dto.response.ProjectListInfoResponse;
 import com.amcamp.domain.team.domain.TeamParticipant;
 import org.springframework.data.domain.Slice;
 
 public interface ProjectRepositoryCustom {
 
-    Slice<ProjectInfoResponse> findAllByTeamIdWithPagination(
-            Long teamId,
-            Long lastProjectId,
-            int pageSize,
-            TeamParticipant teamParticipant,
-            boolean isParticipant);
+    Slice<ProjectListInfoResponse> findAllByTeamIdWithPagination(
+            Long teamId, Long lastProjectId, int pageSize, TeamParticipant teamParticipant);
 }
