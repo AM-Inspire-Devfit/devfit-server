de08fcb862ffd00810299e557771d561bea1be04
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-03-23 01:31:13 +0900

    Revert "refactor: projectListInfoResponse 요청/응답 형식 수정 및 isParticipant, isAdmi…"
    This reverts commit 9ba4a3c4304bc393ca8a45973612e044b169d247.

--- src/main/java/com/amcamp/domain/project/application/ProjectService.java
+++ src/main/java/com/amcamp/domain/project/application/ProjectService.java
@@ -7,7 +7,6 @@ import com.amcamp.domain.project.domain.*;
 import com.amcamp.domain.project.domain.ProjectRegistration;
 import com.amcamp.domain.project.dto.request.*;
 import com.amcamp.domain.project.dto.response.ProjectInfoResponse;
-import com.amcamp.domain.project.dto.response.ProjectListInfoResponse;
 import com.amcamp.domain.project.dto.response.ProjectParticipantInfoResponse;
 import com.amcamp.domain.project.dto.response.ProjectRegistrationInfoResponse;
 import com.amcamp.domain.team.dao.TeamParticipantRepository;
@@ -69,14 +68,14 @@ public class ProjectService {
     }
 
     @Transactional(readOnly = true)
-    public Slice<ProjectListInfoResponse> getProjectListInfo(
-            Long teamId, Long lastProjectId, int pageSize) {
+    public Slice<ProjectInfoResponse> getProjectListInfo(
+            Long teamId, Long lastProjectId, int pageSize, boolean isParticipant) {
 
         Member member = memberUtil.getCurrentMember();
         Team team = getTeam(teamId);
         TeamParticipant teamParticipant = getValidTeamParticipant(member, team);
         return projectRepository.findAllByTeamIdWithPagination(
-                teamId, lastProjectId, pageSize, teamParticipant);
+                teamId, lastProjectId, pageSize, teamParticipant, isParticipant);
     }
 
     // update
@@ -286,5 +285,11 @@ public class ProjectService {
     private boolean isProjectAdmin(Member member, Project project) {
         ProjectParticipant participant = getValidProjectParticipant(member, project);
         return participant.getProjectRole().equals(ProjectParticipantRole.ADMIN);
+    }
+
+    private Boolean isProjectParticipant(Project project, TeamParticipant teamParticipant) {
+        return projectParticipantRepository
+                .findByProjectAndTeamParticipant(project, teamParticipant)
+                .isPresent();
     }
 }
--- src/main/java/com/amcamp/domain/project/dao/ProjectRepositoryCustom.java
+++ src/main/java/com/amcamp/domain/project/dao/ProjectRepositoryCustom.java
@@ -1,11 +1,15 @@
 package com.amcamp.domain.project.dao;
 
-import com.amcamp.domain.project.dto.response.ProjectListInfoResponse;
+import com.amcamp.domain.project.dto.response.ProjectInfoResponse;
 import com.amcamp.domain.team.domain.TeamParticipant;
 import org.springframework.data.domain.Slice;
 
 public interface ProjectRepositoryCustom {
 
-    Slice<ProjectListInfoResponse> findAllByTeamIdWithPagination(
-            Long teamId, Long lastProjectId, int pageSize, TeamParticipant teamParticipant);
+    Slice<ProjectInfoResponse> findAllByTeamIdWithPagination(
+            Long teamId,
+            Long lastProjectId,
+            int pageSize,
+            TeamParticipant teamParticipant,
+            boolean isParticipant);
 }
