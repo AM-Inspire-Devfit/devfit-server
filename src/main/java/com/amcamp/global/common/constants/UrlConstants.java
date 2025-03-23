21e0fd001b1518d932363130e9932b0a7e2a690b
현태 최 <ht3064@naver.com>
2025-03-23 17:30:31 +0900

    chore: 프로파일에 따라 Swagger 서버 URL 동적 설정

--- src/main/java/com/amcamp/global/common/constants/UrlConstants.java
+++ src/main/java/com/amcamp/global/common/constants/UrlConstants.java
@@ -1,6 +1,8 @@
 package com.amcamp.global.common.constants;
 
 public final class UrlConstants {
+    public static final String DEV_SERVER_URL = "https://dev-api.devfit.site";
+    public static final String LOCAL_SERVER_URL = "http://localhost:8080";
 
     public static final String IMAGE_URL = "https://devfit-bucket.s3.ap-northeast-2.amazonaws.com";
 }
--- src/main/java/com/amcamp/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/amcamp/global/config/swagger/SwaggerConfig.java
@@ -1,16 +1,26 @@
 package com.amcamp.global.config.swagger;
 
+import com.amcamp.global.common.constants.UrlConstants;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SwaggerConfig {
+
+    private final Environment environment;
 
     @Bean
     public GroupedOpenApi publicApi() {
@@ -25,8 +35,23 @@ public class SwaggerConfig {
                                 .title("DevFit Server API")
                                 .description("DevFit Server API 명세서입니다.")
                                 .version("v0.0.1"))
+                .servers(getSwaggerServers())
                 .components(authSetting())
                 .addSecurityItem(securityRequirement());
+    }
+
+    private List<Server> getSwaggerServers() {
+        return List.of(new Server().url(resolveServerUrl()));
+    }
+
+    private String resolveServerUrl() {
+        List<String> profiles = List.of(environment.getActiveProfiles());
+
+        if (profiles.contains("dev")) {
+            return UrlConstants.DEV_SERVER_URL;
+        }
+
+        return UrlConstants.LOCAL_SERVER_URL;
     }
 
     private Components authSetting() {
