168d76a34666abeee16879f0165df31e30b49d1c
현태 최 <ht3064@naver.com>
2025-03-23 17:48:18 +0900

    chore: 개발 환경에서 CORS 허용 도메인 설정 추가

--- src/main/java/com/amcamp/global/common/constants/UrlConstants.java
+++ src/main/java/com/amcamp/global/common/constants/UrlConstants.java
@@ -4,5 +4,8 @@ public final class UrlConstants {
     public static final String DEV_SERVER_URL = "https://dev-api.devfit.site";
     public static final String LOCAL_SERVER_URL = "http://localhost:8080";
 
+    public static final String DEV_DOMAIN_URL = "https://www.dev.devfit.site";
+    public static final String LOCAL_DOMAIN_URL = "http://localhost:3000";
+
     public static final String IMAGE_URL = "https://devfit-bucket.s3.ap-northeast-2.amazonaws.com";
 }
