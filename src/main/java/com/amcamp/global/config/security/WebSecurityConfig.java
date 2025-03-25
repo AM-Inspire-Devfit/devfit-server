b87f0efd248bc0ef2a9cce047beb90bbe8f1feb1
현태 최 <ht3064@naver.com>
2025-03-25 13:46:10 +0900

    chore: 피드백 API security 설정

--- src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
@@ -44,6 +44,8 @@ public class WebSecurityConfig {
                 auth ->
                         auth.requestMatchers("/devfit-actuator/**")
                                 .permitAll()
+                                .requestMatchers("/feedbacks/**")
+                                .authenticated()
                                 .requestMatchers("/**")
                                 .permitAll()
                                 .anyRequest()
