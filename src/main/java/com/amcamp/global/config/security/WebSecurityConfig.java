77a8c1d3955aa65ff5a4633c2a8f2361f96ed197
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-03-25 16:39:33 +0900

    chore: 개발 환경 ddl-auto 설정 변경 (#148)
    * chore: 피드백 API security 설정
    
    * chore: 개발 환경 ddl auto update로 변경

--- src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
@@ -45,6 +45,8 @@ public class WebSecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/**")
                                 .permitAll()
+                                .requestMatchers("/feedbacks/**")
+                                .authenticated()
                                 .anyRequest()
                                 .authenticated());
 
