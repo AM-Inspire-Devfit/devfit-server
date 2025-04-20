7d124ed6b36ac4563fb85a0cac8b9dea5c07d042
Subin Cho <chosubinn@ewhain.net>
2025-04-20 19:21:40 +0900

    refactor: sprintIdResponse 내 필드명 수정

--- src/main/java/com/amcamp/domain/sprint/dto/response/SprintIdResponse.java
+++ src/main/java/com/amcamp/domain/sprint/dto/response/SprintIdResponse.java
@@ -3,5 +3,5 @@ package com.amcamp.domain.sprint.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record SprintIdResponse(
-        @Schema(description = "스프린트 ID", example = "1") Long id,
+        @Schema(description = "스프린트 ID", example = "1") Long sprintId,
         @Schema(description = "스프린트 제목", example = "1차 스프린트") String title) {}
