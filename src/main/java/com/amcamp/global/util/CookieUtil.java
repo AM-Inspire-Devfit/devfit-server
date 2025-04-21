c6da86bd0f9a0efb718fa719daeb567b3132c89d
현태 최 <ht3064@naver.com>
2025-04-22 03:05:23 +0900

    fix: prod 환경 쿠키 SameSite 값을 None으로 수정

--- src/main/java/com/amcamp/global/util/CookieUtil.java
+++ src/main/java/com/amcamp/global/util/CookieUtil.java
@@ -2,18 +2,13 @@ package com.amcamp.global.util;
 
 import static com.amcamp.global.common.constants.SecurityConstants.REFRESH_TOKEN_COOKIE_NAME;
 
-import com.amcamp.global.helper.SpringEnvironmentHelper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class CookieUtil {
-
-    private final SpringEnvironmentHelper springEnvironmentHelper;
 
     public HttpHeaders generateRefreshTokenCookie(String refreshToken) {
         ResponseCookie refreshTokenCookie =
@@ -47,9 +42,6 @@ public class CookieUtil {
     }
 
     private String determineSameSitePolicy() {
-        if (springEnvironmentHelper.isProdProfile()) {
-            return Cookie.SameSite.STRICT.attributeValue();
-        }
         return Cookie.SameSite.NONE.attributeValue();
     }
 }
